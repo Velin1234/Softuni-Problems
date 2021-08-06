fef414ed570d46b9331084bf02ba169b7c8215d1
Ludfik <venito1234@abv.bg>
2021-08-06 16:15:52 +0100

    Finished 03.Farming

--- Programming Fundamentals - May 2021/Associative Arrays/Associative Arrays - Exercise/Associative Arrays - Exercise/03.Farming/Program.cs
+++ Programming Fundamentals - May 2021/Associative Arrays/Associative Arrays - Exercise/Associative Arrays - Exercise/03.Farming/Program.cs
@@ -20,9 +20,49 @@ namespace _03.Farming
                 {
                     leftMaterials.Add(materials[i].ToLower(), Convert.ToInt32(materials[i - 1]));
                 }
+
+                if (leftMaterials.ContainsKey("fragments") && leftMaterials["fragments"] >= 250)
+                {
+                    leftMaterials["fragments"] -= 250;
+                    Console.WriteLine("Valanyr obtained!");
+                    break;
+                }
+                if (leftMaterials.ContainsKey("shards") && leftMaterials["shards"] >= 250)
+                {
+                    leftMaterials["shards"] -= 250;
+                    Console.WriteLine("Shadowmourne obtained!");
+                    break;
+                }
+                if (leftMaterials.ContainsKey("motes") && leftMaterials["motes"] >= 250)
+                {
+                    leftMaterials["motes"] -= 250;
+                    Console.WriteLine("Dragonwrath obtained!");
+                    break;
+                }
             }
 
-            foreach (var item in leftMaterials)
+            Dictionary<string, int> mainMaterials = new Dictionary<string, int>();
+            if (leftMaterials.ContainsKey("fragments"))
+            {
+                mainMaterials.Add("fragments", leftMaterials["fragments"]);
+                leftMaterials.Remove("fragments");
+            }
+            if (leftMaterials.ContainsKey("shards"))
+            {
+                mainMaterials.Add("shards", leftMaterials["shards"]);
+                leftMaterials.Remove("shards");
+            }
+            if (leftMaterials.ContainsKey("motes"))
+            {
+                mainMaterials.Add("motes", leftMaterials["motes"]);
+                leftMaterials.Remove("motes");
+            }
+
+            foreach (var main in mainMaterials.OrderByDescending(m => m.Value).ThenBy(m => m.Key))
+            {
+                Console.WriteLine($"{main.Key}: {main.Value}");
+            }
+            foreach (var item in leftMaterials.OrderBy(m => m.Key))
             {
                 Console.WriteLine($"{item.Key}: {item.Value}");
             }
