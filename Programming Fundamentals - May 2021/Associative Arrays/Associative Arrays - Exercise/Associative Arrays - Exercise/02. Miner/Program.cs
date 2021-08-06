0c13468e20246d2c82733c7fa82bbd218ab56b32
Ludfik <venito1234@abv.bg>
2021-08-06 03:30:07 +0100

    Solved 02.Miner

---
+++ Programming Fundamentals - May 2021/Associative Arrays/Associative Arrays - Exercise/Associative Arrays - Exercise/02. Miner/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._Miner
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> ores = new Dictionary<string, int>();
+            string ore = Console.ReadLine();
+            while (ore != "stop")
+            {
+                int quanity = int.Parse(Console.ReadLine());
+                ores.Add(ore, quanity);
+                ore = Console.ReadLine();
+            }
+
+            foreach (var Ore in ores)
+            {
+                Console.WriteLine($"{Ore.Key} -> {Ore.Value}");
+            }
+        }
+    }
+}
