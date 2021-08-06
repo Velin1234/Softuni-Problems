410ea1edacec4e6a93de97d745e8d903f1c841f2
Ludfik <venito1234@abv.bg>
2021-08-06 02:10:01 +0100

    03.Word Synonyms

--- Programming Fundamentals - May 2021/Associative Arrays/Associative Arrays - Lab/03. Word Synonyms/Program.cs
+++ Programming Fundamentals - May 2021/Associative Arrays/Associative Arrays - Lab/03. Word Synonyms/Program.cs
@@ -9,13 +9,26 @@ namespace _03._Word_Synonyms
         {
             int numWords = int.Parse(Console.ReadLine());
             Dictionary<string, List<string>> words = new Dictionary<string, List<string>>();
-            for (int line = 0; line < numWords * 2; line++)
+            for (int line = 0; line < numWords; line++)
             {
                 string word = Console.ReadLine();
                 if (!words.ContainsKey(word))
                 {
                     words.Add(word, new List<string>());
+                    string synonym = Console.ReadLine();
+
+                    words[word].Add(synonym);
                 }
+                else
+                {
+                    string synonym = Console.ReadLine();
+                    words[word].Add(synonym);
+                }
+            }
+
+            foreach (var word in words)
+            {
+                Console.WriteLine($"{word.Key} - {string.Join(", ", word.Value)}");
             }
         }
     }
