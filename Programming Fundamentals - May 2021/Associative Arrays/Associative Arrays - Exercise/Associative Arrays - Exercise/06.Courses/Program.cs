8c3313f55fa077c875a2d80c78ef8d605723bfe9
Ludfik <venito1234@abv.bg>
2021-08-07 03:36:56 +0100

    Added 06.Courses

---
+++ Programming Fundamentals - May 2021/Associative Arrays/Associative Arrays - Exercise/Associative Arrays - Exercise/06.Courses/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.Courses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> studentsInfo = Console.ReadLine().Split(" : ").ToList();
+            Dictionary<string, List<string>> subjectsInfo = new Dictionary<string, List<string>>();
+            while (studentsInfo[0] != "end")
+            {
+                if (subjectsInfo.ContainsKey(studentsInfo[0]))
+                {
+                    subjectsInfo[studentsInfo[0]].Add(studentsInfo[1]);
+
+                }
+                else
+                {
+                    subjectsInfo.Add(studentsInfo[0], new List<string>());
+                    subjectsInfo[studentsInfo[0]].Add(studentsInfo[1]);
+                }
+            }
+        }
+    }
+}
