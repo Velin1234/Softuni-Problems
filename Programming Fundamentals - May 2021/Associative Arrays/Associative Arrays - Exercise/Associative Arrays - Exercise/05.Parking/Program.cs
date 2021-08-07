f47cd16513377f4f6319c2a570e3ef4bbec1d41b
Ludfik <venito1234@abv.bg>
2021-08-07 02:48:03 +0100

    Added 05 Parking

---
+++ Programming Fundamentals - May 2021/Associative Arrays/Associative Arrays - Exercise/Associative Arrays - Exercise/05.Parking/Program.cs
@@ -0,0 +1,54 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.Parking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, string> accounts = new Dictionary<string, string>();
+            for (int i = 0; i < n; i++)
+            {
+                List<string> accountInfo = Console.ReadLine().Split(" ").ToList();
+                string name = accountInfo[1];
+
+                switch (accountInfo[0])
+                {
+                    case "register":
+                        string licensePateNum = accountInfo[2];
+                        if (accounts.ContainsValue(licensePateNum))
+                        {
+                            Console.WriteLine($"ERROR: already registered with plate number {licensePateNum}");
+                        }
+                        else
+                        {
+                            accounts.Add(name, licensePateNum);
+                            Console.WriteLine($"{name} registered {licensePateNum} successfully");
+                        }
+                        break;
+
+                    case "unregister":
+                        if (accounts.ContainsKey(name))
+                        {
+                            accounts.Remove(name);
+                            Console.WriteLine($"{name} unregistered successfully");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"ERROR: user {name} not found");
+                        }
+                        break;
+                }
+            }
+
+            foreach (var acc in accounts)
+            {
+                Console.WriteLine($"{acc.Key} => {acc.Value}");
+            }
+        }
+    }
+}
