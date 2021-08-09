1ea8783730c49b03f9b2c232c12c2a2c3909354e
Ludfik <venito1234@abv.bg>
2021-08-09 17:24:24 +0100

    Added 08.Company

---
+++ Programming Fundamentals - May 2021/Associative Arrays/Associative Arrays - Exercise/Associative Arrays - Exercise/08.Company/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.Company
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> companyInfo = Console.ReadLine().Split(" -> ").ToList();
+            Dictionary<string, List<string>> employeesIDs = new Dictionary<string, List<string>>();
+            while (companyInfo[0] != "End")
+            {
+                if (employeesIDs.ContainsKey(companyInfo[0]))
+                {
+                    employeesIDs[companyInfo[0]].Add(companyInfo[1]);
+
+                }
+                else
+                {
+                    employeesIDs.Add(companyInfo[0], new List<string>());
+                    employeesIDs[companyInfo[0]].Add(companyInfo[1]);
+                }
+                companyInfo = Console.ReadLine().Split(" -> ").ToList();
+            }
+
+            foreach (var subject in employeesIDs.OrderByDescending(c => c.Key))
+            {
+                Console.WriteLine($"{subject.Key}");
+                Console.WriteLine($"-- {string.Join("\n-- ", subject.Value.OrderBy(n => n))}");
+            }
+        }
+    }
+}
