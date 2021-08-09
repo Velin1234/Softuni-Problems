b4bf7f61c86f6839cd69cd844ea39f0490f28408
Ludfik <venito1234@abv.bg>
2021-08-09 21:59:32 +0100

    Added 04. Text_filter

---
+++ Programming Fundamentals - May 2021/Text Processing/Text Processing - Lab/04.Text Filter/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04.Text_Filter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> words = Console.ReadLine().Split(", ").ToList();
+            string text = Console.ReadLine();
+            foreach (var word in words)
+            {
+                string hide = "";
+                for (int i = 0; i < word.Length; i++)
+                {
+                    hide+="*";
+                }
+                text = text.Replace(word, hide);
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
