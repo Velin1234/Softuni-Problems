47bf294ca4d1ae323c8e75b5217ff42adc5c4260
Ludfik <venito1234@abv.bg>
2021-08-09 22:07:13 +0100

    Added 05. Digits,Letters and Others

---
+++ Programming Fundamentals - May 2021/Text Processing/Text Processing - Lab/05. Digits, Leters and Other/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05._Digits__Leters_and_Other
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string digits = "";
+            string letters = "";
+            string symbols = "";
+            for (int symbol = 0; symbol < word.Length; symbol++)
+            {
+                if (char.IsDigit(Convert.ToChar(word[symbol])))
+                {
+                    digits += word[symbol];
+                }
+                else if (char.IsLetter(Convert.ToChar(word[symbol])))
+                {
+                    letters += word[symbol];
+                }
+                else
+                {
+                    symbols += word[symbol];
+                }
+            }
+            Console.WriteLine($"{digits}\n" +
+                $"{letters}\n" +
+                $"{symbols}");
+        }
+    }
+}
