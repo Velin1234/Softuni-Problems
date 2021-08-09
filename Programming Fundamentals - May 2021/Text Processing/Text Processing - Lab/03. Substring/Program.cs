08cf261d8edfec4caf41ee7b152e38799bea0662
Ludfik <venito1234@abv.bg>
2021-08-09 18:36:09 +0100

    Added 03. Substring

---
+++ Programming Fundamentals - May 2021/Text Processing/Text Processing - Lab/03. Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string secondWord = Console.ReadLine();
+            
+            int indexOfWord = secondWord.IndexOf(word);
+            while (indexOfWord >= 0)
+            {
+                secondWord.Remove(indexOfWord, word.Length);
+                indexOfWord = secondWord.IndexOf(word);
+            }
+            Console.WriteLine(secondWord);
+        }
+    }
+}
