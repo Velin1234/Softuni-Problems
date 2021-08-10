169fa0f8a582a4846a7ce0871ea9df881b5f5e0a
Ludfik <venito1234@abv.bg>
2021-08-10 02:03:25 +0100

    Added 01.Valid Username and Text Processing - Exercise

---
+++ Programming Fundamentals - May 2021/Text Processing/Text Processing -Exercise/01.Valid Username/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Valid_Username
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> users = Console.ReadLine().Split(", ",StringSplitOptions.RemoveEmptyEntries).ToList();
+            foreach (var user in users)
+            {
+                if (user.Length < 3 || user.Length > 16)
+                {
+                    continue;
+                }
+                bool isValid = false;
+                foreach (var symbol in user)
+                {
+                    if (!(char.IsDigit(symbol) || char.IsLetter(symbol) || symbol == '_' || symbol == '-'))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                    isValid = true;
+                }
+                if (isValid)
+                {
+                    Console.WriteLine(user);
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - May 2021/Text Processing/Text Processing -Exercise/01.Valid Usernames/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Valid_Usernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> usernames = Console.ReadLine().Split(", ").ToList();
+            
+            foreach (var user in usernames)
+            {
+                if (user.Length >= 3 || user.Length <= 16)
+                {
+                    continue;
+                }
+
+                bool isValid = false;
+                
+                foreach (var symbol in user)
+                {
+                    if (!(char.IsDigit(symbol) || char.IsLetter(symbol) || symbol == '-' || symbol == '_'))
+                    {
+                        isValid = false;
+                        break;
+                    }
+                    isValid = true;
+                }
+
+                if (isValid)
+                {
+                    Console.WriteLine(user);
+                }
+            }
+        }
+    }
+}
