dbf5a6fa31ec694095f8026b89e54879b25a7e53
Ludfik <venito1234@abv.bg>
2021-08-12 12:52:13 +0100

    Added 04.Caesar Cipher

---
+++ Programming Fundamentals - May 2021/Text Processing/Text Processing -Exercise/04. Caesar Cipher/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            StringBuilder password = new StringBuilder();
+            for (int letter = 0; letter < text.Length; letter++)
+            {
+                int newLetter = text[letter] + 3;
+                char passLetter = Convert.ToChar(newLetter);
+                password.Append(passLetter);
+            }
+            Console.WriteLine(password);
+
+        }
+    }
+}
