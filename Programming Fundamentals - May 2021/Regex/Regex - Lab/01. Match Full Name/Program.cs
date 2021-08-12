fa193a6d45fd797d00bb8f61a380ba78cd8d2049
Ludfik <venito1234@abv.bg>
2021-08-12 16:27:58 +0100

    Fixed 01.Match Full name

--- Programming Fundamentals - May 2021/Regex/Regex - Lab/01. Match Full Name/Program.cs
+++ Programming Fundamentals - May 2021/Regex/Regex - Lab/01. Match Full Name/Program.cs
@@ -7,7 +7,7 @@ namespace _01._Match_Full_Name
     {
         static void Main(string[] args)
         {
-            string regex = @"[A-Z][a-z]+ [A-Z][a-z]+";
+            string regex = @"\b[A-Z][a-z]+ [A-Z][a-z]+\b";
             string names = Console.ReadLine();
             MatchCollection matchedNames = Regex.Matches(names, regex);
             foreach (Match name in matchedNames)
