381de6054bb3cb08ee63df959f6ab9a97f000e9b
Ludfik <venito1234@abv.bg>
2021-08-12 12:29:53 +0100

    Added 03.Extract file

---
+++ Programming Fundamentals - May 2021/Text Processing/Text Processing -Exercise/03.Extract file/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Extract_file
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> filePlace = Console.ReadLine().Split("\\").ToList();
+            string [] file = filePlace[filePlace.Count - 1].Split(".");
+            Console.WriteLine($"File name: {file[0]}");
+            Console.WriteLine($"File extension: {file[1]}");
+
+        }
+    }
+}
---
+++ Programming Fundamentals - May 2021/Text Processing/Text Processing -Exercise/03.Extract file/list.cs
@@ -0,0 +1,6 @@
+﻿namespace _03.Extract_file
+{
+    internal class list<T>
+    {
+    }
+}
