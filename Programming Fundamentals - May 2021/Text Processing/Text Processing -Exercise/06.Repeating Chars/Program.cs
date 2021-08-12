abec3ef0c2c829880a5456cb9e258d380d0f2782
Ludfik <venito1234@abv.bg>
2021-08-12 13:10:10 +0100

    Added 06.Repeating Chars

---
+++ Programming Fundamentals - May 2021/Text Processing/Text Processing -Exercise/06.Repeating Chars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+
+namespace _06.Repeating_Chars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder input = new StringBuilder();
+            input.Append(Console.ReadLine());
+            for (int i = 1; i < input.Length; i++)
+            {
+                if (input[i - 1] == input[i])
+                {
+                    input.Remove(i - 1, 1);
+                    i--;
+                }
+            }
+
+            Console.WriteLine(input);
+        }
+    }
+}
