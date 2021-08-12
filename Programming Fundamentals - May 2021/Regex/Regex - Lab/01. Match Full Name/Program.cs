2e2be75f37e533d4f23fa4c4497c3e9215405bbd
Ludfik <venito1234@abv.bg>
2021-08-12 16:22:12 +0100

    Added Regex - Lab and 01.Match Full Name

---
+++ Programming Fundamentals - May 2021/Regex/Regex - Lab/01. Match Full Name/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _01._Match_Full_Name
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string regex = @"[A-Z][a-z]+ [A-Z][a-z]+";
+            string names = Console.ReadLine();
+            MatchCollection matchedNames = Regex.Matches(names, regex);
+            foreach (Match name in matchedNames)
+            {
+                Console.WriteLine($"{name.Value}" +" ");
+            }
+        }
+    }
+}
