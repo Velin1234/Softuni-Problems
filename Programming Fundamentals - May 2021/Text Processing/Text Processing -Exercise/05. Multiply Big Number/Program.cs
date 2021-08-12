6741462d3d280dd803774aee645abc9fe4438d38
Ludfik <venito1234@abv.bg>
2021-08-12 13:02:12 +0100

    Added 05.Multiply Big Num

---
+++ Programming Fundamentals - May 2021/Text Processing/Text Processing -Exercise/05. Multiply Big Number/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Numerics;
+
+namespace _05._Multiply_Big_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BigInteger bigNum = BigInteger.Parse(Console.ReadLine());
+            int multiplier = int.Parse(Console.ReadLine());
+            Console.WriteLine(bigNum * multiplier);
+        }
+    }
+}
