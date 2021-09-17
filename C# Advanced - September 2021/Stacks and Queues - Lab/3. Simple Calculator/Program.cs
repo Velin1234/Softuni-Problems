9260446d899ff3dd91059b046761718b1481e24a
Velin3020@students.softuni.bg <Velin3020@students.softuni.bg>
2021-09-17 18:09:41 +0300

    Solved 3. Simple Calculator

--- C# Advanced - September 2021/Stacks and Queues - Lab/3. Simple Calculator/Program.cs
+++ C# Advanced - September 2021/Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _3._Simple_Calculator
 {
@@ -6,7 +8,32 @@ namespace _3._Simple_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<string> equation = new Stack<string>();
+            string[] input = Console.ReadLine().Split(" ").ToArray();
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                equation.Push(Convert.ToString(input[i]));
+            }
+            int result = Convert.ToInt32(equation.Peek());
+            equation.Pop();
+
+            while (equation.Count != 0)
+            {
+                if (equation.Peek() == "+")
+                {
+                    equation.Pop();
+                    result += Convert.ToInt32(equation.Peek());
+                    equation.Pop();
+                }
+                else if (equation.Peek() == "-")
+                {
+                    equation.Pop();
+                    result -= Convert.ToInt32(equation.Peek());
+                    equation.Pop();
+                }
+            }
+            
+            Console.WriteLine(result);
         }
     }
 }
