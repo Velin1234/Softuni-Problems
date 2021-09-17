2506b2eb28a88f14941a9792e9688814de65e251
Velin3020@students.softuni.bg <Velin3020@students.softuni.bg>
2021-09-17 17:39:44 +0300

    Solved 2.Stack Sum

---
+++ C# Advanced - September 2021/Stacks and Queues - Lab/2. Stack Sum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2._Stack_Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> numbers = new Stack<int>();
+            int[] inputNums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int sum = 0;
+            foreach (var num in inputNums)
+            {
+                numbers.Push(num);
+            }
+            
+            List<string> cmd = Console.ReadLine().Split(" ").ToList();
+            while (cmd[0].ToLower() != "end")
+            {
+                switch (cmd[0].ToLower())
+                {
+                    case "add":
+                        numbers.Push(Convert.ToInt32(cmd[1]));
+                        numbers.Push(Convert.ToInt32(cmd[2]));
+                        break;
+                    case "remove":
+                        if (numbers.Count >= Convert.ToInt32(cmd[1]))
+                        {
+                            for (int i = 0; i < Convert.ToInt32(cmd[1]); i++)
+                            {
+                                numbers.Pop();
+                            }
+                        }
+                        break;
+                }
+                cmd = Console.ReadLine().Split(" ").ToList();
+            }
+
+            foreach (var num in numbers)
+            {
+                sum += num;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
---
+++ C# Advanced - September 2021/Stacks and Queues - Lab/3. Simple Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _3._Simple_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
