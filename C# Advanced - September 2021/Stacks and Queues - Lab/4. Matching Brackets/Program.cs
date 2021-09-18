3bc8e78641f518ee63ebe2a10cc0d859bbdb2326
Velin3020@students.softuni.bg <Velin3020@students.softuni.bg>
2021-09-18 21:02:43 +0300

    Solved 4.Matching brackets

---
+++ C# Advanced - September 2021/Stacks and Queues - Lab/4. Matching Brackets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4._Matching_Brackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string arithmeticExpr = Console.ReadLine();
+            Stack<int> indices = new Stack<int>();
+            for (int i = 0; i < arithmeticExpr.Length; i++)
+            {
+                if (arithmeticExpr[i] == '(')
+                {
+                    indices.Push(i);
+                }
+                else if (arithmeticExpr[i] == ')')
+                {
+                    var openBrackedIndex = indices.Pop();
+                    var closedBrackedIndex = i;
+                    Console.WriteLine(arithmeticExpr.Substring(openBrackedIndex, closedBrackedIndex - openBrackedIndex + 1));
+                }
+            }
+        }
+    }
+}
