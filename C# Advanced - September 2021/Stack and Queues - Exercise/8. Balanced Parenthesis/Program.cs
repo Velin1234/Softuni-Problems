8e77324f17c77c75d820690b75dbfdfcd49600b0
Velin3020@students.softuni.bg <Velin3020@students.softuni.bg>
2021-09-21 16:18:53 +0300

    Finished Stack and Queries - Exercise

---
+++ C# Advanced - September 2021/Stack and Queues - Exercise/8. Balanced Parenthesis/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _8._Balanced_Parenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Stack<char> parantheses = new Stack<char>();
+            bool isBalanced = true;
+            foreach (var symbol in input)
+            {
+                if (symbol == '{' || symbol == '[' || symbol == '(')
+                {
+                    parantheses.Push(symbol);
+                }
+                else
+                {
+                    if (parantheses.Count == 0)
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+
+                    bool isValid = (symbol == '}' && parantheses.Peek() == '{') 
+                        || (symbol == ']' && parantheses.Peek() == '[') 
+                        || (symbol == ')' && parantheses.Peek() == '(');
+
+                    if (isValid)
+                    {
+                        parantheses.Pop();
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                    }
+                }
+                
+            }
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+        }
+
+    }
+}
---
+++ C# Advanced - September 2021/Stack and Queues - Exercise/9. Simple Text Editor/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _9._Simple_Text_Editor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberRotations = int.Parse(Console.ReadLine());
+            StringBuilder text = new StringBuilder();
+            Stack<string> pushedPhrases = new Stack<string>();
+            for (int i = 0; i < numberRotations; i++)
+            {
+                string[] command = Console.ReadLine().Split();
+                switch (command[0])
+                {
+                    case "1":
+                        pushedPhrases.Push(text.ToString());
+                        string strToAppend = command[1];
+                        text.Append(strToAppend);
+                        break;
+                    case "2":
+                        pushedPhrases.Push(text.ToString());
+                        int countChars = int.Parse(command[1]);
+                        text.Remove(text.Length - countChars, countChars);
+                        break;
+                    case "3":
+                        int index = int.Parse(command[1]);
+                        Console.WriteLine(text[index - 1]);
+                        break;
+                    case "4":
+                        if (text.Length == 0)
+                        {
+                            text.Append(pushedPhrases.Pop());
+                        }
+                        else
+                        {
+                            text.Replace(text.ToString(), pushedPhrases.Pop());
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
