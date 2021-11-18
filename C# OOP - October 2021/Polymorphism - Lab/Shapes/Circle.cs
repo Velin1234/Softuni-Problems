f1132c15227341ffdc83027054778227441ce195
Velin3020@students.softuni.bg <Velin3020@students.softuni.bg>
2021-11-18 17:08:15 +0200

    Finished Polymorphism - Lab

---
+++ C# OOP - October 2021/Polymorphism - Lab/Shapes/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    public class Circle : Shape
+    {
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get;protected set; }
+        public override double CalculateArea()
+        {
+            return Math.PI * Math.Pow(Radius, 2);
+        }
+        public override double CalculatePerimeter()
+        {
+            return 2 * Radius * Math.PI;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw()+ GetType().Name;
+        }
+    }
+}
---
+++ C# OOP - October 2021/Polymorphism - Lab/Shapes/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rectange = new Rectangle(10,20);
+            Shape circle = new Circle(10);
+            Console.WriteLine(rectange.CalculatePerimeter());
+            Console.WriteLine(rectange.CalculateArea());
+            Console.WriteLine(rectange.Draw());
+            Console.WriteLine(circle.CalculatePerimeter());
+            Console.WriteLine(circle.CalculateArea());
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
